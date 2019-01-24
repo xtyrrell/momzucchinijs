d37adfa42a879795ea92816293c23b0e731fc452
Max <max.dez@live.co.za>
2019-01-24 22:10:24 +0200

    Add CLI and start by default

--- main.js
+++ main.js
@@ -2,11 +2,19 @@ import readline from 'readline'
 
 import evaluate from './lib/mom-please-get-me-so-me-zucchini-from-sho-p.js'
 
-const rl = readline.createInterface({input: process.stdin, output: process.stdout, prompt: '-> hey mom '})
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '-> hey mom '
+})
 
+rl.write('hello welcom e to mom please get zucchine thax. please start  program ok\n')
 rl.prompt()
 
+let state
+
 rl.on('line', line => {
-  evaluate(line)
+  if (!state) state = evaluate(line)
+  else state = evaluate(line, state)
   rl.prompt()
 })
