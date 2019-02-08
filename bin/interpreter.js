37248eadc61dfbb447c85f81e9041ccc61d4668c
Max <max.dez@live.co.za>
2019-02-08 14:12:54 +0200

    Move examples to their own directory

--- bin/interpreter.js
+++ bin/interpreter.js
@@ -1,15 +1,39 @@
-#!/usr/bin/env node
+import readline from 'readline'
+import path from 'path'
+import fs from 'fs'
 
-const readline = require('readline')
+import evaluate from '../lib/evaluate.js'
 
-const cancelScript = require('../lib/cancelscript')
+let interactive = true
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+for (const arg of process.argv) {
+  if (path.extname(arg) === '.zucchinipleasemom') {
+    interactive = false
+    evaluate(fs.readFileSync(path.normalize(arg)))
+  }
+}
 
-rl.on('line', line => {
-  cancelScript(line)
-  rl.close()
-})
+if (interactive) createInteractiveCLI()
+
+function createInteractiveCLI () {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '-> hey mom '
+  })
+
+  console.log('hello welcom e to mom please get zucchine thax. please start  program ok')
+
+  rl.prompt()
+
+  let state
+
+  rl.on('line', line => {
+    if (!state) {
+      state = evaluate(line)
+      state.interactive = true
+    }
+    else state = evaluate(line, state)
+    rl.prompt()
+  })
+}
