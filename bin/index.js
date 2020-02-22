8d1df8bde1d35af5fce757b660c280600f4a5e55
Max Tyrrell <maxtyrrell0@gmail.com>
2020-02-22 20:08:08 +0200

    Convert CommonJS to ES Modules

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
+import path from 'path'
+import fs from 'fs'
 
-// Enable ESM 'import' and 'export' stuff with the `esm` module.
-require = require("esm")(module)
-module.exports = require("./interpreter.js")
+import evaluate from '../lib/evaluate.js'
+import setupInteractiveCli from './interpreter.js'
+
+let interactive = true
+
+for (const arg of process.argv) {
+  if (path.extname(arg) === '.zucchinipleasemom') {
+    interactive = false
+    evaluate(fs.readFileSync(path.normalize(arg)))
+  }
+}
+
+if (interactive) setupInteractiveCli()
--- bin/interpreter.js
+++ bin/interpreter.js
@@ -1,28 +1,17 @@
 import readline from 'readline'
-import path from 'path'
-import fs from 'fs'
 
 import evaluate from '../lib/evaluate.js'
 
-let interactive = true
-
-for (const arg of process.argv) {
-  if (path.extname(arg) === '.zucchinipleasemom') {
-    interactive = false
-    evaluate(fs.readFileSync(path.normalize(arg)))
-  }
-}
-
-if (interactive) createInteractiveCLI()
-
-function createInteractiveCLI () {
+export default function setupInteractiveCli () {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     prompt: '-> hey mom '
   })
 
-  console.log('hello welcom e to mom please get zucchine thax. please start  program ok')
+  console.log(
+    'hello welcom e to mom please get zucchine thax. please start  program ok'
+  )
 
   rl.prompt()
 
@@ -32,8 +21,7 @@ function createInteractiveCLI () {
     if (!state) {
       state = evaluate(line)
       state.interactive = true
-    }
-    else state = evaluate(line, state)
+    } else state = evaluate(line, state)
     rl.prompt()
   })
 }
